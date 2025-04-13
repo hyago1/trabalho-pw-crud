b5574da1267c0acbb5352a8bf6337d94768164bb
Hyago Philip <hyagophilip@gmail.com>
2025-04-13 12:31:43 -0300

    first commit

---
+++ src/main/java/com/tads/pw/trabalhodepw/TrabalhodepwApplication.java
@@ -0,0 +1,13 @@
+package com.tads.pw.trabalhodepw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhodepwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhodepwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tads/pw/trabalhodepw/TrabalhodepwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tads.pw.trabalhodepw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhodepwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
